f2e3c4327392b937a10b545f41cc155c07a0650b
Igor Leal <igorleal96@gmail.com>
2023-08-16 14:54:31 -0300

    validação de formulário ( edittext )

--- app/src/main/java/com/example/myinstagram/LoginActivity.kt
+++ app/src/main/java/com/example/myinstagram/LoginActivity.kt
@@ -2,10 +2,14 @@ package com.example.myinstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
