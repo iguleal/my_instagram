faf4ef9a1de3ffae4a326687603ca364eb2cf6bc
Igor Leal <igorleal96@gmail.com>
2023-08-16 15:20:29 -0300

    ViewBinding adicionado e botão Entrar dinâmico

--- app/src/main/java/com/example/myinstagram/LoginActivity.kt
+++ app/src/main/java/com/example/myinstagram/LoginActivity.kt
@@ -1,15 +1,37 @@
 package com.example.myinstagram
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import com.google.android.material.textfield.TextInputLayout
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myinstagram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.editEmail.addTextChangedListener(watcher)
+        binding.editPassword.addTextChangedListener(watcher)
+
+        binding.btnEnter.setOnClickListener {
+            binding.editEmailInput.error = "Email Inválido"
+            binding.editPasswordInput.error = "Senha Inválida"
+        }
+    }
+    private val watcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            binding.btnEnter.isEnabled = s.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+        }
 
     }
 }
