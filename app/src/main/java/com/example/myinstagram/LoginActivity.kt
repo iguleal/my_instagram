44d61703070056d9619773973a2d7c10e216f8e1
Igor Leal <igorleal96@gmail.com>
2023-08-23 15:25:19 -0300

    button_enter com animação de progressBar

--- app/src/main/java/com/example/myinstagram/LoginActivity.kt
+++ app/src/main/java/com/example/myinstagram/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myinstagram
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
@@ -18,8 +20,14 @@ class LoginActivity : AppCompatActivity() {
         binding.editPassword.addTextChangedListener(watcher)
 
         binding.btnEnter.setOnClickListener {
+            binding.btnEnter.showProgressBar(true)
+
             binding.editEmailInput.error = "Email Inválido"
             binding.editPasswordInput.error = "Senha Inválida"
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                binding.btnEnter.showProgressBar(false)
+            }, 2000)
         }
     }
     private val watcher = object : TextWatcher {
