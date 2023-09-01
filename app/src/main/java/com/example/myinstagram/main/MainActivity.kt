cc6cf9c685f5e2c4a71125bafa3f210f5c73eff7
Igor Leal <igorleal96@gmail.com>
2023-09-01 18:06:22 -0300

    Fragment Profile

---
+++ app/src/main/java/com/example/myinstagram/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myinstagram.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myinstagram.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/example/myinstagram/register/RegisterPhotoFragment.kt
+++ app/src/main/java/com/example/myinstagram/register/RegisterPhotoFragment.kt
@@ -33,9 +33,7 @@ class RegisterPhotoFragment : Fragment() {
 
                 }
             }
-
         }
-
         customDialog.show()
     }
 
