f86a6fd663204d8bdf59bf434647caf4e6e3f3d3
Igor Leal <igorleal96@gmail.com>
2023-09-02 18:07:50 -0300

    fragments de camera e gallery

---
+++ app/src/main/java/com/example/myinstagram/camera/CameraFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myinstagram.camera
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myinstagram.R
+
+class CameraFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+
+}
