3b0149acc9fb0cb4855d0fc0649100be4923c223
Igor Leal <igorleal96@gmail.com>
2023-09-29 15:09:35 -0300

    foto da galeria funcionando e trocando a view

--- app/src/main/java/com/example/myinstagram/common/view/CropperImageFragment.kt
+++ app/src/main/java/com/example/myinstagram/common/view/CropperImageFragment.kt
@@ -4,10 +4,14 @@ import android.net.Uri
 
 import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.setFragmentResult
 import com.example.myinstagram.R
 import com.example.myinstagram.databinding.FragmentImageCropperBinding
+import java.io.File
 
 class CropperImageFragment : Fragment(R.layout.fragment_image_cropper) {
     private var binding: FragmentImageCropperBinding? = null
@@ -32,8 +36,18 @@ class CropperImageFragment : Fragment(R.layout.fragment_image_cropper) {
                 }
 
                 btnSaveCropper.setOnClickListener {
-                    ///////////////////////////////PAREI AQUI //////////////////////////////////////////////
+                    val dir = context?.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+                    if (dir != null){
+                        val uriToSaved = Uri.fromFile(File(dir.path, System.currentTimeMillis().toString() + ".jpeg"))
+                        containerCropper.saveCroppedImageAsync(uriToSaved)
+                    }
                 }
+
+                containerCropper.setOnCropImageCompleteListener { view, result ->
+                    setFragmentResult("cropKey", bundleOf(KEY_URI to result.uri))
+                    parentFragmentManager.popBackStack()
+                }
+
             }
         }
     }
--- app/src/main/java/com/example/myinstagram/register/view/RegisterPhotoFragment.kt
+++ app/src/main/java/com/example/myinstagram/register/view/RegisterPhotoFragment.kt
@@ -1,10 +1,14 @@
 package com.example.myinstagram.register.view
 
 import android.content.Context
+import android.net.Uri
+import android.os.Build.VERSION.SDK_INT
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.setFragmentResultListener
 import com.example.myinstagram.R
+import com.example.myinstagram.common.view.CropperImageFragment.Companion.KEY_URI
 import com.example.myinstagram.common.view.CustomDialog
 import com.example.myinstagram.databinding.FragmentRegisterPhotoBinding
 
@@ -13,6 +17,19 @@ class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo) {
 
     private var binding: FragmentRegisterPhotoBinding? = null
     private var fragmentAttachListener: FragmentAttachListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setFragmentResultListener("cropKey"){ requestKey, bundle ->
+            val uri = when{
+                SDK_INT >= 33 -> bundle.getParcelable(KEY_URI, Uri::class.java)
+                else -> bundle.getParcelable(KEY_URI)
+            }
+            onCropImageResult(uri)
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentRegisterPhotoBinding.bind(view)
@@ -50,6 +67,12 @@ class RegisterPhotoFragment : Fragment(R.layout.fragment_register_photo) {
         super.onAttach(context)
         if (context is FragmentAttachListener){
             fragmentAttachListener = context
+        }
+    }
+
+    private fun onCropImageResult(uri: Uri?){
+        if (uri != null){
+            binding?.imgRegisterProfile?.setImageURI(uri)
         }
     }
 
